996bc790f568046c1e62cf39c3d0bd03d3d97c69
Rodrigo <rodrigo.costa2015@engenharia.ufjf.br>
2021-07-03 18:46:43 -0300

    feat(historias): finalização Florianopolis

--- Historias.py
+++ Historias.py
@@ -6,6 +6,7 @@ import os
 
 
 monstros = lib.CriaMonstros()
+#armas = lib.CriaArmas()
 
 def Intro(Heroi):
     arq = "Arquivostxt/Introducao.txt"
@@ -144,16 +145,125 @@ def CapituloFloripaMago(Heroi):
         lib.LimpaConsole()
         lib.ProcuraTexto("R2-2-p1","R2-2-fim",arq, Heroi.name)
         lib.LimpaConsole()
-        print("Literalmente lanchado.")
+        print("Literalmente lanchado.") #GameOver
         sys.exit()
     elif(int(resp) == 3):
         lib.ProcuraTexto("R2-3-ini", "R2-3-p1", arq, Heroi.name)
+        lib.LimpaConsole()
+        lib.ProcuraTexto("R2-3-p1", "R2-3-p2", arq, Heroi.name)
+        lib.LimpaConsole()
+        lib.Combate(Heroi,lib.GetMonstro(monstros, "Willhelm"))
+        lib.ProcuraTexto("R2-3-p2", "R2-3-fim", arq, Heroi.name)
+        lib.LimpaConsole()
+        Heroi.HP = Heroi.HPmax
+        Heroi.MP = Heroi.MPmax
     #endif
-    lib.ProcuraTexto("Q3-ini", "Q2-p1", arq,Heroi.name)
+    lib.ProcuraTexto("Q3-ini", "Q3-p1", arq,Heroi.name)
     lib.LimpaConsole()
-    lib.ProcuraTexto("Q2-p1", "Q2-p2", arq,Heroi.name)
+    lib.ProcuraTexto("Q3-p1", "Q3-fim", arq,Heroi.name)
+    resp = input()
+    os.system("cls")
+    if(int(resp) == 1):
+        lib.ProcuraTexto("R3-1-ini", "R3-1-p1", arq,Heroi.name)
+        lib.LimpaConsole()
+        lib.ProcuraTexto("R3-1-p1", "R3-1-p2", arq,Heroi.name)
+        lib.LimpaConsole()
+        lib.ProcuraTexto("R3-1-p2", "R3-1-p3", arq,Heroi.name)
+        lib.LimpaConsole()
+        lib.ProcuraTexto("R3-1-p3", "R3-1-p4", arq,Heroi.name)
+        lib.LimpaConsole()
+        lib.ProcuraTexto("R3-1-p4", "R3-1-p5", arq,Heroi.name)
+        lib.LimpaConsole()
+        lib.ProcuraTexto("R3-1-p5", "R3-1-p6", arq,Heroi.name)
+        lib.LimpaConsole()
+        lib.Combate(Heroi,lib.GetMonstro(monstros, "Demonio Inferior"))
+        lib.ProcuraTexto("R3-1-p6", "R3-1-p7", arq,Heroi.name)
+        lib.LimpaConsole()
+        lib.ProcuraTexto("R3-1-p7", "R3-1-p8", arq,Heroi.name)
+        lib.LimpaConsole()
+        lib.ProcuraTexto("R3-1-p8", "R3-1-p9", arq,Heroi.name)
+        lib.LimpaConsole()
+        lib.ProcuraTexto("R3-1-p9", "R3-1-fim", arq,Heroi.name)
+        lib.LimpaConsole()
+    elif(int(resp) == 2):
+        lib.ProcuraTexto("R3-2-ini", "R3-2-p1", arq,Heroi.name)
+        lib.LimpaConsole()
+        lib.ProcuraTexto("R3-2-p1", "R3-2-p2", arq,Heroi.name)
+        lib.LimpaConsole()
+        lib.ProcuraTexto("R3-2-p2", "R3-2-p3", arq,Heroi.name)
+        lib.LimpaConsole()
+        lib.Combate(Heroi,lib.GetMonstro(monstros, "Demonio Inferior"))
+        lib.ProcuraTexto("R3-2-p3", "R3-2-p4", arq,Heroi.name)
+        lib.LimpaConsole()
+        lib.ProcuraTexto("R3-2-p4", "R3-2-p5", arq,Heroi.name)
+        lib.LimpaConsole()
+        lib.ProcuraTexto("R3-2-p5", "R3-2-fim", arq,Heroi.name)
+        lib.LimpaConsole()
+        #Heroi.arma = lib.GetArma(armas,"arma1mago")
+        #print("Voce recebeu uma nova arma: ", Heroi.arma)
+    elif(int(resp) == 3):
+        lib.ProcuraTexto("R3-3-ini", "R3-3-p1", arq,Heroi.name)
+        lib.LimpaConsole()
+        lib.ProcuraTexto("R3-3-p1", "R3-3-p2", arq,Heroi.name)
+        lib.LimpaConsole()
+        lib.ProcuraTexto("R3-3-p2", "R3-3-p3", arq,Heroi.name)
+        lib.LimpaConsole()
+        lib.Combate(Heroi,lib.GetMonstro(monstros, "Demonio Inferior"))
+        lib.ProcuraTexto("R3-3-p3", "R3-3-fim", arq,Heroi.name)
+        lib.LimpaConsole()
+    #endif
+    lib.ProcuraTexto("Q4-ini", "Q4-p1", arq,Heroi.name)
+    lib.LimpaConsole()
+    lib.ProcuraTexto("Q4-p1", "Q4-p2", arq,Heroi.name)
+    lib.LimpaConsole()
+    lib.ProcuraTexto("Q4-p2", "Q4-p3", arq,Heroi.name)
+    lib.LimpaConsole()
+    lib.ProcuraTexto("Q4-p3", "Q4-p4", arq,Heroi.name)
+    lib.LimpaConsole()
+    lib.ProcuraTexto("Q4-p4", "Q4-fim", arq,Heroi.name)
+    resp = input()
+    os.system("cls")
+    if(int(resp) == 1):
+        lib.ProcuraTexto("R4-1-ini", "R4-1-p1", arq,Heroi.name)
+        lib.LimpaConsole()
+        lib.ProcuraTexto("R4-1-p1", "R4-1-p2", arq,Heroi.name)
+        lib.LimpaConsole()
+        lib.ProcuraTexto("R4-1-p2", "R4-1-p3", arq,Heroi.name)
+        lib.LimpaConsole()
+        lib.ProcuraTexto("R4-1-p3", "R4-1-p4", arq,Heroi.name)
+        lib.LimpaConsole()
+        lib.ProcuraTexto("R4-1-p4", "R4-1-p5", arq,Heroi.name)
+        lib.LimpaConsole()
+        lib.ProcuraTexto("R4-1-p5", "R4-1-fim", arq,Heroi.name)
+        lib.LimpaConsole()
+        CapituloKrambeckMago(Heroi)
+    elif(int(resp) == 2):
+        lib.ProcuraTexto("R4-2-ini", "R4-2-p1", arq,Heroi.name)
+        lib.LimpaConsole()
+        lib.ProcuraTexto("R4-2-p1", "R4-2-p2", arq,Heroi.name)
+        lib.LimpaConsole()
+        lib.ProcuraTexto("R4-2-p2", "R4-2-p3", arq,Heroi.name)
+        lib.LimpaConsole()
+        lib.Combate(Heroi,lib.GetMonstro(monstros, "Guarda Magico de Elite"))
+        lib.LimpaConsole()
+        lib.Combate(Heroi,lib.GetMonstro(monstros, "Guarda Magico de Elite"))
+        lib.ProcuraTexto("R4-2-p3", "R4-2-fim", arq,Heroi.name)
+        lib.LimpaConsole()
+        CapituloFelastusMago(Heroi)
+    #endif
     
-    
+def CapituloFelastusMago(Heroi):
+    print("")
+
+def CapituloFelastusGuerreiro(Heroi):
+    print("")    
+
+def CapituloKrambeckArqueiro(Heroi):
+    print("")
+
+def CapituloKrambeckMago(Heroi):
+    print("")
+
 def IntroGuerreiro(Heroi):
     print("")
 
